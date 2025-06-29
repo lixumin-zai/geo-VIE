581b164ac1b22580bc52dae4e2d0cf75c7db3bf2
lismin <lixumin1030@gmail.com>
2025-06-29 23:43:26 +0800

    feat: add

--- data/src/scene.py
+++ data/src/scene.py
@@ -3,3 +3,13 @@
 # @Author  :   lixumin1030@gmail.com
 # @FileName:   scene.py
 
+
+from manim import *
+
+
+def GenScene(Scene):
+    def __init__(self):
+        pass
+
+    def construct(self):
+        pass
